b961dbdabd5282bfb65e99f08cb6388808e9efb9
Ryuichi Yamashita <poloswim@gmail.com>
2016-06-08 04:42:02 +0900

    facebook17 login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,6 +17,6 @@ Devise.setup do |config|
   config.omniauth :google_oauth2, '598316334523-ubn4slrtha366romc00eg7e89p4o6f5b.apps.googleusercontent.com', 'RAS7xhRbKL0cvS8KySCBd5J1', {access_type: "offline", approval_prompt: ""}
 
   require 'omniauth-facebook'
-  config.omniauth :facebook, '733820026760459', '418896a99cdf79a7a680628dd3796a61'
+  config.omniauth :facebook, '733820026760459', '418896a99cdf79a7a680628dd3796a61',  scope: 'email', info_fields: 'email,name'
 
 end
