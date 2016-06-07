175f6bd128be0831ff203b12c936e48d247ebe9d
Ryuichi Yamashita <poloswim@gmail.com>
2016-06-08 04:29:00 +0900

    facebook15 login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 	def avatar_url(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
 		if user.image
-			user.image
+		  user.image
 		else
-			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=50"
+		  "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=40"
 		end
 	end
 end
