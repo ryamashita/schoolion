615e62ab6c9971bea274fb7e48b1edc48b5b003d
Ryuichi Yamashita <poloswim@gmail.com>
2016-06-08 00:58:11 +0900

    facebook8 login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:google_oauth2]
+         :omniauthable
 
   validates :name, presence: true, length: {maximum: 25}
 
