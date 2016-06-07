5417e9523dba949134f1443170986b10664d0a2a
Ryuichi Yamashita <poloswim@gmail.com>
2016-06-08 04:05:53 +0900

    facebook13 login

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,7 @@ class User < ActiveRecord::Base
         user.email = auth.info.email
         user.image = auth.info.image
         user.password = Devise.friendly_token[0,20]
+    end
   end
 
 end
