c159071110bdccd034f0b8597cdc3b01944a3043
Ryuichi Yamashita <poloswim@gmail.com>
2016-06-08 00:50:22 +0900

    facebook7 login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'http://www.schoolion.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :user_name => 'app51698963@heroku.com',
+  :password => 'r4mizasr4482',
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => 'login',
+  :enable_starttls_auto => true
+  }
+
+
 end
