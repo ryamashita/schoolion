cc390810f16b542300ab287a5c82da36a80ee34f
Ryuichi Yamashita <poloswim@gmail.com>
2016-06-08 02:02:52 +0900

    facebook10 login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -13,19 +13,19 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	end
 
 	def facebook
-		@user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+		@user = User.from_facebook_omniauth(request.env["omniauth.auth"])
 
 		if @user.persisted?
-			flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-			sign_in_and_redirect @user, :event => :authentication
+		  sign_in_and_redirect @user, :event => :authentication
+		  set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
 		else
-			session["devise.google_data"] = request.env["omniauth.auth"]
-			redirect_to new_user_registration_url
-		end
-	end
+		  session["devise.facebook_data"] = request.env["omniauth.auth"]
+		  redirect_to new_user_registration_url
+    	end
+    end
 
     def twitter
-		@user = User.from_twitter_omniauth(request.env["omniauth.auth"])
+		@user = User.from_twiter_omniauth(request.env["omniauth.auth"])
 
 		if @user.persisted?
 		  sign_in_and_redirect @user, :event => :authentication
