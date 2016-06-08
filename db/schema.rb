56252b4e1f8b9139f5d38366ef50234a01a9f8e2
Ryuichi Yamashita <poloswim@gmail.com>
2016-06-09 05:18:40 +0900

    change active admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608192518) do
+ActiveRecord::Schema.define(version: 20160608201522) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 20160608192518) do
     t.text     "note"
     t.string   "video"
     t.boolean  "header",     default: false, null: false
-    t.string   "tag"
+    t.integer  "tag"
     t.integer  "project_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
